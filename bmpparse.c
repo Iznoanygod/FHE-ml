68f0fb80d54f9fd5f74bec722d11fab026bc5dbe
iznoanygod <mkbandrewp@gmail.com>
2022-04-26 01:47:45 -0400

    parser

--- bmpparse.c
+++ bmpparse.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int reverse_bytes(int x) {
+    int build = (x & 0xFF) << 24;
+    build += (x & 0xFF00) << 8;
+    build += (x & 0xFF0000) >> 8;
+    build += (x & 0xFF000000) >> 24;
+    return build;
+}
+
 double *parse_bmp(char *file_name) {
     FILE *input_file;
     input_file = fopen(file_name, "r");
@@ -19,16 +27,20 @@ double *parse_bmp(char *file_name) {
     for(int i = 0; i < 28; i++) {
         int row;
         fread(&row, 4, 1, input_file);
+        row = reverse_bytes(row);
         row = row >> 4;
         for(int j = 0; j < 28; j++) {
             if(row & 0x1 == 1)
                 mat[27-j][27-i] = 0.0;
             else
                 mat[27-j][27-i] = 1.0;
+            row = row >> 1;
         }
+
     }
     for(int i = 0; i < 28*28; i++) {
         vector[i] = mat[i%28][i/28];
     }
     return vector;
 }
+
